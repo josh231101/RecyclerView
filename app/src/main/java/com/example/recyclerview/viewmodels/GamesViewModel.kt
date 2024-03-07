7b421107ea2ee6b515ac50aa5e4c32971d3f9f06
josh231101 <wiijosue333@gmail.com>
2024-03-06 18:46:28 -0600

    build game view model

---
+++ app/src/main/java/com/example/recyclerview/viewmodels/GamesViewModel.kt
@@ -0,0 +1,29 @@
+package com.example.recyclerview.viewmodels
+
+import androidx.lifecycle.ViewModel
+import com.example.recyclerview.R
+import com.example.recyclerview.models.Game
+
+class GamesViewModel: ViewModel() {
+    init {
+
+    }
+
+    fun getGameList(): ArrayList<Game> {
+        val game:Game = Game("Mario vs Sonic", 1600, "PS4", R.drawable.mario)
+        val gameList: ArrayList<Game> = ArrayList<Game>()
+        gameList.add(game)
+        gameList.add(Game("Mario", 1000, "Wii", R.drawable.mario))
+        gameList.add(Game("Mario 2", 1000, "Wii", R.drawable.mario))
+        gameList.add(Game("Mario 3", 1000, "Wii", R.drawable.mario))
+        gameList.add(Game("Mario 4", 1000, "Wii", R.drawable.mario))
+        gameList.add(Game("Mario 5", 1000, "Wii", R.drawable.mario))
+        gameList.add(Game("Mario 6", 1000, "Wii", R.drawable.mario))
+        gameList.add(Game("Mario 7", 1000, "Wii", R.drawable.mario))
+        gameList.add(Game("Mario 8", 1000, "Wii", R.drawable.mario))
+        gameList.add(Game("Mario 9", 1000, "Wii", R.drawable.mario))
+        gameList.add(Game("Mario 10", 1000, "Wii", R.drawable.mario))
+        return gameList
+    }
+
+}
